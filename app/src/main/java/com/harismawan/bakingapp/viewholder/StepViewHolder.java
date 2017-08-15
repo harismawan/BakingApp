caf1eeec719d01738c9bb7c1c1fd8a445e9eba8c
harismawan <achmadfauziharismawan@gmail.com>
2017-08-15 15:27:13 +0700

    add ExoPlayer library to play video

--- app/src/main/java/com/harismawan/bakingapp/viewholder/StepViewHolder.java
+++ app/src/main/java/com/harismawan/bakingapp/viewholder/StepViewHolder.java
@@ -1,12 +1,24 @@
 package com.harismawan.bakingapp.viewholder;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.VideoView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.harismawan.bakingapp.R;
 import com.squareup.picasso.Picasso;
 import eu.davidea.flexibleadapter.FlexibleAdapter;
@@ -14,7 +26,7 @@ import eu.davidea.viewholders.FlexibleViewHolder;
 
 public class StepViewHolder extends FlexibleViewHolder {
 
-    @BindView(R.id.step_video) VideoView video;
+    @BindView(R.id.step_video) SimpleExoPlayerView video;
     @BindView(R.id.step_image) ImageView image;
     @BindView(R.id.step_short) TextView shortDesc;
     @BindView(R.id.step_desc) TextView desc;
@@ -29,6 +41,7 @@ public class StepViewHolder extends FlexibleViewHolder {
 
     public void bind(String video, String image, String shortDesc, String desc) {
         if (!video.isEmpty()) {
+            initPlayer(video);
 
         }
 
@@ -39,5 +52,20 @@ public class StepViewHolder extends FlexibleViewHolder {
 
         this.shortDesc.setText(shortDesc);
         this.desc.setText(desc);
+    }
+
+    private void initPlayer(String url) {
+        video.setVisibility(View.VISIBLE);
+
+        TrackSelector trackSelector = new DefaultTrackSelector();
+        LoadControl loadControl = new DefaultLoadControl();
+        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
+        video.setPlayer(player);
+
+        String userAgent = Util.getUserAgent(context, context.getString(R.string.app_name));
+        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse(url), new DefaultDataSourceFactory
+                (context, userAgent), new DefaultExtractorsFactory(), null, null);
+        player.prepare(mediaSource);
+        player.setPlayWhenReady(false);
     }
 }
